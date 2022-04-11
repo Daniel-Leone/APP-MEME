0051cc8580a4ef73740559141b78e0ad4738f1ff
Daniel Leone <danielleone60@gmail.com>
2022-04-11 14:59:44 -0300

    página de memes terminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import html2canvas from 'html2canvas'
 
 function App() {
+
+  const [linea1, setLinea1] = useState();
+  const [linea2, setLinea2] = useState();
+  const [imagen, setImagen] = useState();
+  const [color, setColor] = useState();
+
+  const setLinea1Function = (valor)=>{
+    setLinea1(valor.target.value)
+  }
+
+  const setLinea2Function = (valor)=>{
+    setLinea2(valor.target.value)
+  }
+
+  const setImagenFunction = (valor)=>{
+    setImagen(valor.target.value)
+  }
+
+  const setTextColor = (valorColor)=>{
+    setColor(valorColor.target.value)
+  }
+
+  const exportarImagen = ()=>{
+    html2canvas(document.querySelector(".memeContainer")).then(canvas => {
+      let img = canvas.toDataURL("image/png");
+      let link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Seleccione el meme: </span>
+      <select onChange={setImagenFunction}>
+        <option value='fire'>fire</option>
+        <option value='futurama'>futurama</option>
+        <option value='history'>history</option>
+        <option value='matrix'>matrix</option>
+        <option value='philosoraptor'>philosoraptor</option>
+        <option value='smart'>smart</option>
+      </select> <br/>
+
+      <input onChange={setLinea1Function} type='text'/> <br/>
+      <input onChange={setLinea2Function} type='text'/> <br/>
+      <p>Color: <input onChange={setTextColor} type='color'/></p>
+
+      <div className='memeContainer'>
+        <span className='span1' style={{color: color}}>{linea1}</span>
+
+        <img src={`/images/${imagen}.jpg`}/>
+
+        <span className='span2' style={{color: color}}>{linea2}</span>
+      </div>
+      
+      <button onClick={exportarImagen}>Exportar</button>
     </div>
   );
 }
